87f66f5875525940aeead44be1c788efb35efa03
Robin Andersson <robin@binf.ku.dk>
2019-03-27 12:59:08 +0100

    adding gene complexity function

--- complexity.R
+++ complexity.R
@@ -2,7 +2,7 @@ require("CAGEfightR")
 
 source("CAGEfightR_extensions/utils.R")
 
-calcComplexity <- function(object, inputAssay = "counts", step=1e6, unexpressed=0) {
+calcCTSSComplexity <- function(object, inputAssay = "counts", step=1e6, unexpressed=0) {
 
     object <- calcTotalTags(object, inputAssay=inputAssay)
 
@@ -17,6 +17,29 @@ calcComplexity <- function(object, inputAssay = "counts", step=1e6, unexpressed=
 
         df <- data.frame(target=t,sample=colnames(x),totalTags=x$totalTags,numberCTSSs=x$numberCTSSs)
         df[object$totalTags<t,c("totalTags","numberCTSSs")] <- NA
+
+        df
+    })
+    
+    do.call("rbind",res)
+}
+
+calcGeneComplexity <- function(object, txModels, inputAssay = "counts", step=1e6, unexpressed=0) {
+
+    object <- calcTotalTags(object, inputAssay=inputAssay)
+    object <- assignGeneID(object, geneModels = txModels, outputColumn = "geneID")
+
+    targets <- seq(step,max(object$totalTags),by=step)
+
+    res <- lapply(targets, function(t) {
+        message("subsampling to ",t,"...")
+
+        x <- subsampleTarget(object, inputAssay, t)
+        x <- calcNumberGenes(x, txModels, inputAssay, unexpressed = unexpressed)
+        x <- calcTotalTags(x, inputAssay=inputAssay)
+
+        df <- data.frame(target=t,sample=colnames(x),totalTags=x$totalTags,numberGenes=x$numberGenes)
+        df[object$totalTags<t,c("totalTags","numberGenes")] <- NA
 
         df
     })
