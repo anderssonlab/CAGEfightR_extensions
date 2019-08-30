3cc8ca2da7fd9e931cd410f72b7dafc23b0fe5b8
Robin Andersson <robin@binf.ku.dk>
2019-08-30 17:59:41 +0200

    average weighted MSE functions added

--- variance.R
+++ variance.R
@@ -2,7 +2,7 @@ require("CAGEfightR")
 
 ## Object: GRanges
 ## ctss: RangedSummarizedExperiment
-CTSSvariation <- function(object, ctss, inputAssay="counts", outputColumn="MSE", standardize=TRUE, fn=mean_squared_error) {
+CTSSvariation <- function(object, ctss, inputAssay="counts", outputColumn="MSE", fn=mean_squared_error) {
 
     ## Find overlaps
     message("Finding overlaps...")
@@ -15,13 +15,12 @@ CTSSvariation <- function(object, ctss, inputAssay="counts", outputColumn="MSE",
     ctss <- ctss[ids,]
     ids <- split(1:nrow(ctss), hits[-which(is.na(hits))])
 
-    ## Calculate variances
+    ## Calculate variation
     message("Calculating variation...")
     
     data <- assay(ctss, inputAssay)
-    value <- unlist(bplapply(ids[1:11], function(x) {
-        d <- scale(data[x,], center=standardize, scale=standardize)
-        fn(d)
+    value <- unlist(bplapply(ids, function(x) {
+        fn(data[x,])
     }))
 
     mcols(object)[, outputColumn] <- value
@@ -30,17 +29,55 @@ CTSSvariation <- function(object, ctss, inputAssay="counts", outputColumn="MSE",
 }
 
 mean_squared_error <- function(d) {
-    val <- which(apply(d, 2, function(x) !(any(is.na(x)))))
-    cent <- rowMeans(d[,val])
-    sum(apply(d[,val], 2, function(x) sum((x - cent)^2))) / nrow(d)
+    d <- as.matrix(d)
+    d <- scale(d, center=TRUE, scale=TRUE)
+    cent <- rowMeans(d,na.rm=TRUE)
+    mean(apply(d, 2, function(x) sum((x - cent)^2)) / nrow(d), na.rm=TRUE)
+}
+
+weighted_mean_squared_error <- function(d) {
+    d <- as.matrix(d)
+    p <- colSums(d) / sum(d)
+    d <- scale(d, center=TRUE, scale=TRUE)
+    cent <- rowMeans(d,na.rm=TRUE)
+    sum(p*apply(d, 2, function(x) sum((x - cent)^2)) / nrow(d), na.rm=TRUE)
+}
+
+random_mean_squared_error <- function(d) {
+    d <- as.matrix(d)
+    t <- rowSums(d)
+    n <- nrow(d)
+    p <- colSums(d) / sum(t)
+    
+    mean(sapply(1:10, function(i) {
+        ## Subsample the pooled counts to each sample's proportion of the total
+        d <- scale(sapply(p, function(x) rbinom(n, t, x)), center=TRUE, scale=TRUE)
+        cent <- rowMeans(d,na.rm=TRUE)
+        mean(apply(d, 2, function(x) sum((x - cent)^2, na.rm=TRUE)) / nrow(d), na.rm=TRUE)
+    }), na.rm=TRUE)
+}
+
+random_weighted_mean_squared_error <- function(d) {
+    d <- as.matrix(d)
+    t <- rowSums(d)
+    n <- nrow(d)
+    p <- colSums(d) / sum(t)
+    
+    mean(sapply(1:10, function(i) {
+        ## Subsample the pooled counts to each sample's proportion of the total
+        d <- scale(sapply(p, function(x) rbinom(n, t, x)), center=TRUE, scale=TRUE)
+        cent <- rowMeans(d,na.rm=TRUE)
+        sum(p*apply(d, 2, function(x) sum((x - cent)^2, na.rm=TRUE)) / nrow(d), na.rm=TRUE)
+    }), na.rm=TRUE)
 }
 
 average_divergence <- function(d) {
-    val <- which(apply(d, 2, function(x) !(any(is.na(x)))))
-    cent <- rowMeans(d[,val])
+    d <- as.matrix(d)
+    cent <- rowMeans(d)
     cent <- cent / sum(cent)
-    
-    sum(apply(d[,val], 2, function(x) {
+
+    ## Average Jensen-Shannon divergence versus the cluster centroid
+    mean(apply(d, 2, function(x) {
         x <- x / sum(x)
         m <- 0.5 * (cent + x)
         z <- 0.5 * (x * log2(x/m) + cent * log2(cent/m))
@@ -49,6 +86,32 @@ average_divergence <- function(d) {
         z[z == Inf] <- 0
         z[is.na(z)] <- 0
         sqrt(sum(z))
-    })) / length(val)
+    }))
 }
 
+random_divergence <- function(d) {
+    d <- as.matrix(d)
+    t <- rowSums(d)
+    n <- nrow(d)
+    p <- colSums(d) / sum(t)
+
+    mean(sapply(1:10, function(i) {
+        ## Subsample the pooled counts to each sample's proportion of the total
+        d <- sapply(p, function(x) rbinom(n, t, x))
+        
+        t <- rowMeans(d)
+        cent <- t / sum(t)
+        
+        ## Average Jensen-Shannon divergence versus the cluster centroid
+        mean(apply(d, 2, function(x) {
+            x <- x / sum(x)
+            m <- 0.5 * (cent + x)
+            z <- 0.5 * (x * log2(x/m) + cent * log2(cent/m))
+            
+            z[z == -Inf] <- 0
+            z[z == Inf] <- 0
+            z[is.na(z)] <- 0
+            sqrt(sum(z))
+        }))
+    }), na.rm=TRUE)
+}
