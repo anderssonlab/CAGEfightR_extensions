509ef8cc24c814ebe927d1242f7d7d3919519bfe
Robin Andersson <robin@binf.ku.dk>
2020-09-15 16:25:27 +0200

    adding correlation profile function

---
+++ correlation.R
@@ -0,0 +1,35 @@
+source("CAGEfightR_extensions/heatmap.R")
+
+## Calculates the auto correlation of CTSSs over tag clusters according to pooled values
+## and the cross correlation with opposite strand CTSSs
+
+## object: GRanges
+## pooled: RangedSummarizedExperiment
+corrprofiles <- function(object, pooled, flank=300, max.lag=300) {
+
+    ## flank TCs around their summits
+    start(object) <- start(object$thick)-flank
+    end(object) <- start(object$thick)+flank
+
+    ## retrieve data around regions
+    message("Retrieving data")
+    data <- heatmapData(object, pooled)
+
+    message("Calculating correlation profiles")
+    strand <- strand=as.character(strand(object))
+    sense_matrix <- matrix(0, ncol=2*flank+1, nrow=length(object))
+    antisense_matrix <- matrix(0, ncol=2*flank+1, nrow=length(object))
+
+    sense_matrix[strand=="+",] <- data[["+"]][["+"]]
+    sense_matrix[strand=="-",] <- data[["-"]][["-"]][,(2*flank+1):1]
+    antisense_matrix[strand=="+",] <- data[["+"]][["-"]]
+    antisense_matrix[strand=="-",] <- data[["-"]][["+"]][,(2*flank+1):1]
+
+    ac_matrix <- t(sapply(1:length(object), function(i) acf(sense_matrix[i,],plot=FALSE,max.lag=max.lag)$acf[,1,1]))
+    cc_matrix <- t(sapply(1:length(object), function(i) ccf(sense_matrix[i,],antisense_matrix[i,],plot=FALSE,max.lag=max.lag)$acf[,1,1]))
+
+    rownames(ac_matrix) <- names(object)
+    rownames(cc_matrix) <- names(object)
+
+    return(list(ac=ac_matrix,cc=cc_matrix))
+}
