b08a36e16acb87424abf70fb763b9d243972300d
Robin Andersson <robin@binf.ku.dk>
2020-11-13 17:39:51 +0100

    adding DM adjusted CV

--- dispersion.R
+++ dispersion.R
@@ -1,8 +1,12 @@
 require("CAGEfightR")
+require("DescTools")
 
 ## Object: GRanges
 ## ctss: RangedSummarizedExperiment
-CTSSdispersion <- function(object, ctss, inputAssay="counts", outputColumn="TSS_MADM", fn=tss_madm, ...) {
+CTSSdispersion <- function(object, ctss, inputAssay="TPM", outputColumn="TSS_GINI", fn, ...) {
+
+    assertthat::assert_that(isSorted(ctss))
+    assertthat::assert_that(isSorted(object))
 
     ## Find overlaps
     message("Finding overlaps...")
@@ -20,7 +24,7 @@ CTSSdispersion <- function(object, ctss, inputAssay="counts", outputColumn="TSS_
 
     data <- assay(ctss, inputAssay)
     value <- unlist(bplapply(ids, function(x) {
-        fn(data[x,],...)
+        fn(data[x,,drop=FALSE],...)
     }))
 
     mcols(object)[, outputColumn] <- value
@@ -29,7 +33,7 @@ CTSSdispersion <- function(object, ctss, inputAssay="counts", outputColumn="TSS_
 }
 
 ## object: RangedSummarizedExperiment
-TCdispersion <- function(object, inputAssay="counts", outputColumn="TC_MADM", fn=tc_madm, ...) {
+TCdispersion <- function(object, inputAssay="TPM", outputColumn="TC_GINI", fn=tc_gini, ...) {
 
     message("Calculating TC dispersion...")
 
@@ -55,19 +59,150 @@ calcMedian <- function(object, inputAssay="counts", outputColumn="median") {
 }
 
 ## object: RangedSummarizedExperiment
-calcMean <- function(object, inputAssay="counts", outputColumn="mean") {
+calcMax <- function(object, inputAssay="counts", outputColumn="median") {
 
     data <- assay(object, inputAssay)
-    value <- rowMeans(data,na.rm=TRUE)
+    value <- apply(data,1,max,na.rm=TRUE)
 
     mcols(object)[, outputColumn] <- value
 
     object
 }
 
+## object: RangedSummarizedExperiment
+calcMean <- function(object, inputAssay="counts", outputColumn="mean") {
+
+    data <- assay(object, inputAssay)
+    value <- Matrix::rowMeans(data,na.rm=TRUE)
+
+    mcols(object)[, outputColumn] <- value
+
+    object
+}
+
+TCdispersionData <- function(object, ctss, inputAssay="TPM", fn=tss_disp_data, ...) {
+
+    assertthat::assert_that(isSorted(ctss))
+    assertthat::assert_that(isSorted(object))
+
+    ## Find overlaps
+    message("Finding overlaps...")
+
+    hits <- findOverlaps(query = ctss,
+                         subject = object,
+                         select = "arbitrary")
+    hits <- factor(hits, levels=seq_along(object))
+    ids <- as.numeric(unlist(split(1:length(hits), hits)))
+    ctss <- ctss[ids,]
+    ids <- split(1:nrow(ctss), hits[-which(is.na(hits))])
+
+    ## Calculate dispersion
+    message("Extracting data...")
+
+    data <- assay(ctss, inputAssay)
+    value <- bplapply(ids, function(x) {
+        fn(data[x,,drop=FALSE],...)
+    })
+
+    value
+}
+
+tss_disp_data <- function(d, pseudo=rep(0,ncol(d)), max_zero_prop=1) {
+    d <- as.matrix(d)
+    if (nrow(d)==1)
+        return(0)
+    if (max_zero_prop < 1)
+    {
+        propz <- apply(d,1,function(x) sum(x==0))/ncol(d)
+        idx <- which(propz <= max_zero_prop)
+    }
+    d.pseudo <- t(t(d)+pseudo)
+    dnorm <- apply(d.pseudo,2,function(x) x/sum(x))
+    list(expr=colSums(d)+pseudo,
+         orig=d.pseudo[idx,,drop=FALSE],
+         norm=dnorm[idx,,drop=FALSE])
+}
+
+rowVars <- function(x, ...) {
+    sqr <- function(x)  x*x
+    n <- Matrix::rowSums(!is.na(x))
+    n[n<=1] <- NA
+    return(rowSums(sqr(x-Matrix::rowMeans(x, ...)), ...)/(n-1))
+}
+
+rowSds <- function(x, ...)
+      sqrt(rowVars(x, ...))
+
+calcCV <- function(object, inputAssay="counts", outputColumn="CV") {
+
+    data <- assay(object, inputAssay)
+    value <- rowSds(data,na.rm=TRUE)/Matrix::rowMeans(data,na.rm=TRUE)
+
+    mcols(object)[, outputColumn] <- value
+
+    object
+}
+
+associateWithTC <- function(object, ctss) {
+
+    assertthat::assert_that(isSorted(ctss))
+    assertthat::assert_that(isSorted(object))
+
+    hits <- findOverlaps(query = ctss,
+                         subject = object,
+                         select = "arbitrary")
+    hits <- factor(hits, levels=seq_along(object))
+    ids <- as.numeric(unlist(split(1:length(hits), hits)))
+    ctss <-  ctss[ids,]
+    mcols(ctss)[,"TC"] <- rownames(object)[hits[-which(is.na(hits))]]
+
+    ctss
+}
+
+DMadjustedCV <- function(object, inputAssay="TPM", prefix="") {
+
+    require(zoo)
+
+    object <- calcCV(object, inputAssay, paste(prefix,"CV",sep=""))
+    object <- calcMean(object, inputAssay, paste(prefix,"mean",sep=""))
+    mcols(object)[,paste(prefix,"log10_CV2",sep="")] <- log10(rowData(object)[,paste(prefix,"CV",sep="")]^2)
+
+    mean_order <- order(rowData(object)[,paste(prefix,"mean",sep="")])
+
+    rolling_median <- rollapply(mcols(object)[,paste(prefix,"log10_CV2",sep="")][mean_order],width=50,by=25,FUN=median,fill=list("extend","extend","NA"))
+    nas <- which(is.na(rolling_median))
+    rolling_median[nas] <- median(mcols(object)[,paste(prefix,"log10_CV2",sep="")][mean_order][nas])
+    names(rolling_median) <- (1:length(object))[mean_order]
+    reorder <- match(1:length(object),names(rolling_median))
+    rolling_median <- rolling_median[reorder]
+    mcols(object)[, paste(prefix,"roll_median_log10_CV2",sep="")] <- rolling_median
+
+    mcols(object)[, paste(prefix,"adjusted_log10_CV2",sep="")] <- mcols(object)[,paste(prefix,"log10_CV2",sep="")] - rolling_median
+
+    object
+}
+
+DMadjustedUsageCV <- function(ctss, TCcolumn="TC", inputAssay="counts") {
+
+    data <- assay(ctss,inputAssay)
+    TCs <- mcols(ctss)[,TCcolumn]
+    for (TC in unique(TCs)) {
+        idx <- which(TCs==TC)
+        if (length(idx)>0)
+            data[idx,] <- Matrix::t(Matrix::t(data[idx,])/(Matrix::colSums(data[idx,])/1000000))
+    }
+
+    mcols(ctss)[,"usage_TPM"] <- data
+
+    DMadjustedCV(ctss,inputAssay="usage_TPM",prefix="usage_")
+}
+
 ## Object: GRanges
 ## ctss: RangedSummarizedExperiment
-numCTSSs <- function(object, ctss, inputAssay="counts", outputColumn="num_CTSSs", max_zero_prop=0.5) {
+numCTSSs <- function(object, ctss, inputAssay="TPM", outputColumn="num_CTSSs", max_zero_prop=1) {
+
+    assertthat::assert_that(isSorted(ctss))
+    assertthat::assert_that(isSorted(object))
 
     ## Find overlaps
     message("Finding overlaps...")
@@ -85,7 +220,7 @@ numCTSSs <- function(object, ctss, inputAssay="counts", outputColumn="num_CTSSs"
 
     data <- assay(ctss, inputAssay)
     value <- unlist(bplapply(ids, function(x) {
-        x <- as.matrix(x)
+        x <- as.matrix(data[x,,drop=FALSE])
         sum(apply(x,1,function(y) sum(y==0))/ncol(x) <= max_zero_prop)
     }))
 
@@ -94,32 +229,23 @@ numCTSSs <- function(object, ctss, inputAssay="counts", outputColumn="num_CTSSs"
     object
 }
 
+tc_gini <- function(d, pseudo=rep(0,length(d)), max_zero_prop=0.9) {
+    d <- as.numeric(d)
+    if (max_zero_prop < 1)
+    {
+        propz <- sum(d==0)/length(d)
+        if (propz > max_zero_prop)
+            return(NA)
+    }
+    d <- d+pseudo
+    Gini(d)
+}
+
 madm <- function(x) {
     m <- median(x,na.rm=TRUE)
     if (m==0)
         return(NA)
     median(abs(x-m),na.rm=TRUE)/m
-}
-
-tss_madm <- function(d, pseudo=rep(0,ncol(d)), max_zero_prop=0.5) {
-    d <- as.matrix(d)
-    if (nrow(d)==1)
-        return(0)
-    if (max_zero_prop < 1)
-    {
-        propz <- apply(d,1,function(x) sum(x==0))/ncol(d)
-        idx <- which(propz <= max_zero_prop)
-    }
-    else
-        idx <- 1:nrow(d)
-    if (length(idx)==0)
-        return(NA)
-    if (length(idx)==1)
-        return(0)
-    d <- t(t(d)+pseudo)
-    dnorm <- matrix(0,nrow=nrow(d),ncol=ncol(d))
-    dnorm[idx,] <- apply(d[idx,,drop=FALSE],2,function(x) x/sum(x))
-    median(apply(dnorm[idx,,drop=FALSE], 1, madm),na.rm=TRUE)
 }
 
 tc_madm <- function(d, pseudo=rep(0,length(d)), max_zero_prop=0.5) {
@@ -132,37 +258,4 @@ tc_madm <- function(d, pseudo=rep(0,length(d)), max_zero_prop=0.5) {
     }
     d <- d+pseudo
     madm(d)
-}
-
-mean_squared_error <- function(d) {
-    d <- as.matrix(d)
-    d <- scale(d, center=TRUE, scale=TRUE)
-    cent <- rowMeans(d,na.rm=TRUE)
-    mean(apply(d, 2, function(x) sum((x - cent)^2)) / nrow(d), na.rm=TRUE)
-}
-
-weighted_mean_squared_error <- function(d) {
-    d <- as.matrix(d)
-    p <- colSums(d) / sum(d)
-    d <- scale(d, center=TRUE, scale=TRUE)
-    cent <- rowMeans(d,na.rm=TRUE)
-    sum(p*apply(d, 2, function(x) sum((x - cent)^2)) / nrow(d), na.rm=TRUE)
-}
-
-average_divergence <- function(d) {
-    d <- as.matrix(d)
-    cent <- rowMeans(d)
-    cent <- cent / sum(cent)
-
-    ## Average Jensen-Shannon divergence versus the cluster centroid
-    mean(apply(d, 2, function(x) {
-        x <- x / sum(x)
-        m <- 0.5 * (cent + x)
-        z <- 0.5 * (x * log2(x/m) + cent * log2(cent/m))
-
-        z[z == -Inf] <- 0
-        z[z == Inf] <- 0
-        z[is.na(z)] <- 0
-        sqrt(sum(z))
-    }))
 }
